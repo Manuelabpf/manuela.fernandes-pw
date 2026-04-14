5b3b4d69478ab450c8e685653b7d79a1ff1f19ac
Manuela Fernandes <manubpf@gmail.com>
2026-04-14 08:05:39 -0300

    Add files via upload

---
+++ aula1404/js/nome.js
@@ -0,0 +1,9 @@
+var nome = "Gustavo";
+var nome2 = prompt("Digite seu nome: ")
+var idade = parseInt(prompt("Digite sua idade: "));
+
+
+console.log("Seu nome é: ", nome);
+console.log("Sua idade é :", idade);
+alert("Seu nome é: "+nome2);
+alert("Sua idade é: "+idade);
