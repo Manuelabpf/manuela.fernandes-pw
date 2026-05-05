97cbc5bea8657bae01b701585734dd27e9a6d2fa
Manuela Fernandes <manubpf@gmail.com>
2026-05-05 08:08:42 -0300

    Add files via upload

---
+++ Aula0505/js/nome.js
@@ -0,0 +1,9 @@
+//passando por todas as palavras
+var palavra = "MANUELA";
+var i=0;
+//passando por todos os elementos 
+while(i < 7){
+
+    console.log(palavra[i]);
+    i++
+}
---
+++ Aula0505/js/script.js
@@ -0,0 +1,10 @@
+    // Estrutura de repetiçao while
+    var x = 0;
+    var soma = 0;
+ while(x<=5){
+     soma = soma + x
+         //Incrementador
+   x++
+
+ }
+console.log(soma);
